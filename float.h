50c5eae032b4ae5fba54064e38001dfc6831c9dc
Sowdambika <146941994+Sowdambika@users.noreply.github.com>
2023-10-14 06:57:39 +0200

    Create float.h
    basic program

---
+++ float.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <float.h>
+
+int main()
+{
+    printf("float precision=%d\n",FLT_DIG);
+     printf("double precision=%d\n",DBL_DIG);
+      printf("long double precision=%d\n",LDBL_DIG);
+}
